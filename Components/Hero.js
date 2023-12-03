e3cce18372be0885cf5d8a33c30ffc8c243b895f
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-12-03 22:30:38 +0530

    📱 Make component responsive for all devices

--- Components/Hero.js
+++ Components/Hero.js
@@ -12,10 +12,10 @@ const Hero = () => {
   const [checkOutDate, setCheckOutDate] = useState(null);
 
   return (
-    <section className="max-w-screen-xl mx-auto mt-28">
-      <div className="flex flex-col items-center justify-between lg:flex-row">
+    <section className="max-w-screen-xl mx-auto mt-28 px-4">
+      <div className="grid lg:grid-cols-2 gap-y-8">
         {/* Left side */}
-        <div className="flex-1 p-4 max-w-lg">
+        <div className="flex-1 p-4 lg:max-w-lg">
           <h2 className="text-3xl text-center lg:text-left font-bold mb-8">
             <span>Discover </span>
             <span className="bg-gradient-to-r from-textgrad_1 to-textgrad_2 inline-block text-transparent bg-clip-text">
@@ -26,9 +26,9 @@ const Hero = () => {
           <p className="text-gray-600 mb-12 text-center lg:text-left text-2xl">
             {text.hero_subtext}
           </p>
-          <div className="flex flex-col sm:flex-row gap-4">
+          <div className="grid grid-cols-2 max-w-max gap-4 mx-auto">
             {/* Check-in section */}
-            <div className="mb-4 flex-1">
+            <div className="col-span-2 sm:col-span-1">
               <label className="block text-sm font-semibold text-gray-700 mb-1">
                 Check-in:{" "}
               </label>
@@ -40,12 +40,12 @@ const Hero = () => {
                 calendarClassName="p-0"
                 showIcon
                 calendarIconClassname="absolute !pt-2.5 right-0"
-                className="w-full !pr-32 lg:!pr-20    font-left !py-1.5 !pl-2 items-center font-light border border-gray-300 placeholder-placeColor rounded focus:outline-none"
+                className="!w-full !pr-32 lg:!pr-20 font-left !py-1.5 !pl-2 items-center font-light border border-gray-300 placeholder-placeColor rounded focus:outline-none"
               />
             </div>
 
             {/* Check-out section */}
-            <div className="mb-4 flex-1">
+            <div className="col-span-2 sm:col-span-1">
               <label className="block text-sm font-semibold text-gray-700 mb-1">
                 Check-out:{" "}
               </label>
@@ -60,28 +60,28 @@ const Hero = () => {
                 className="w-full !pr-32 lg:!pr-20    font-left !py-1.5 !pl-2 items-center font-light border border-gray-300 placeholder-placeColor rounded focus:outline-none"
               />
             </div>
+
+            <button className="w-full py-3 px-4 rounded bg-gradient-to-r from-btngrad_1 to-btngrad_2 bg-opacity-90 hover:bg-opacity-100 text-white text-sm font-semibold flex items-center justify-center gap-1 font-urbanist col-span-2">
+              <Image
+                src={Img.SVG__ICONS.B_ICON}
+                width={18}
+                height={18}
+                alt="image"
+              />
+              {text.nav_btn}
+            </button>
           </div>
-          <button className="w-full py-3 px-4 rounded bg-gradient-to-r from-btngrad_1 to-btngrad_2 bg-opacity-90 hover:bg-opacity-100 text-white text-sm font-semibold flex items-center justify-center gap-1 font-urbanist">
-            <Image
-              src={Img.SVG__ICONS.B_ICON}
-              width={18}
-              height={18}
-              alt="image"
-            />
-            {text.nav_btn}
-          </button>
         </div>
 
         {/* Right side */}
-        <div className=" w-1/2 p-4">
-          <Image
-            width={570}
-            height={564}
-            src={Img.home_vectors.card}
-            alt="card"
-            className="rounded h-full w-full "
-          />
-        </div>
+        <Image
+          width="0"
+          height="0"
+          sizes="100vw"
+          src={Img.home_vectors.card}
+          alt="card"
+          className="w-full"
+        />
       </div>
     </section>
   );
--- Components/ImageGrid.js
+++ Components/ImageGrid.js
@@ -6,21 +6,17 @@ import Image from "next/image";
 
 const ImageGrid = ({ images }) => {
   return (
-    <div className="flex flex-wrap mt-8 mx-4">
+    <div className="grid md:grid-cols-2 lg:grid-cols-4 flex-wrap mt-8 mx-4 max-w-screen-xl lg:mx-auto">
       {imgAPI.home_img.map((image, index) => (
-        <div
+        <Image
+          width="0"
+          height="0"
+          sizes="100vw"
+          src={image}
+          alt={`Image ${index + 1}`}
+          className="w-full h-auto rounded"
           key={index}
-          className="w-full sm:w-1/2 lg:w-1/4 lg:w-1/4 xl:w-1/4 p-4 "
-        >
-          <Image
-            width="0"
-            height="0"
-            sizes="100vw"
-            src={image}
-            alt={`Image ${index + 1}`}
-            className="w-full h-auto rounded"
-          />
-        </div>
+        />
       ))}
     </div>
   );
