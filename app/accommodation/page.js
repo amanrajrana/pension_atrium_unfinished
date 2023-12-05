e01ce7b3cd2a4dd950275f81433bc8dd92452ff5
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-12-06 00:46:23 +0530

    ⏺️ center the <main> element

--- app/accommodation/page.js
+++ app/accommodation/page.js
@@ -6,7 +6,7 @@ import Information from "@/Components/Information";
 
 export default function Page() {
   return (
-    <main className="font-urbanist max-w-screen-2xl px-4 lg:px-10">
+    <main className="font-urbanist mx-auto max-w-screen-2xl px-4 lg:px-10">
       <Tables />
       <Floor heading="Ground Floor" desc={text.first_floor} />
       <Floor heading="First Floor" desc={text.ground_floor} />
