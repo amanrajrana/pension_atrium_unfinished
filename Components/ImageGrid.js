cd3ac3d940bbd0c294f1ed7131702e29ab163851
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-12-06 01:16:36 +0530

    ✨ Update ImageGrid component  images

--- Components/ImageGrid.js
+++ Components/ImageGrid.js
@@ -6,15 +6,15 @@ import Image from "next/image";
 
 const ImageGrid = ({ images }) => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 flex-wrap mt-8 mx-auto max-w-screen-2xl px-4 lg:px-6 gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-4 flex-wrap mt-4 mx-auto max-w-screen-2xl px-4 lg:px-6 gap-4">
       {imgAPI.home_img.map((image, index) => (
         <Image
-          width="0"
-          height="0"
+          width="836"
+          height="418"
           sizes="100vw"
           src={image}
           alt={`Image ${index + 1}`}
-          className="w-full h-auto rounded-xl bg-red-500"
+          className="w-full h-full rounded-xl object-cover"
           key={index}
         />
       ))}
