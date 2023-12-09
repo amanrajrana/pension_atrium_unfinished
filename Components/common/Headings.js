798260d0aeeef408a016aa87bc74f97c8043c856
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-12-09 19:45:09 +0530

    💡 update heading font size for small screens

--- Components/common/Headings.js
+++ Components/common/Headings.js
@@ -5,7 +5,7 @@ const H1 = ({ className, ...props }) => {
   return (
     <h1
       className={cn(
-        "text-4xlg 2xl:text-4xl 3xl:text-5xl font-bold leading-normal",
+        "text-3xl sm:text-4xlg 2xl:text-4xl 3xl:text-5xl font-bold leading-normal",
         className
       )}
       {...props}
@@ -17,7 +17,7 @@ const H2 = ({ className, ...props }) => {
   return (
     <h2
       className={cn(
-        "xl:text-3xl 2xl:text-4xl 3xl:text-5xl font-bold leading-normal",
+        "text-2xl xl:text-3xl 2xl:text-4xl 3xl:text-5xl font-bold leading-normal",
         className
       )}
       {...props}
