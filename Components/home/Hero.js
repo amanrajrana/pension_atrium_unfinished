e70804b24c69622cb58c00fdcb3539888546eeec
Aman Raj Rana <amanrajrana.dev@gmail.com>
2023-12-09 19:10:56 +0530

    💄 Refactor date input field component width and padding

--- Components/home/Hero.js
+++ Components/home/Hero.js
@@ -1,17 +1,16 @@
-// Hero.js
 "use client";
-import React, { useState } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+
 import Image from "next/image";
 import Img from "@/public/images/ImageApi";
 import text from "@/public/text.json";
 import { BSymbolIcon } from "@/Components/svg/icons";
 import { Button } from "@/Components/common/button";
+import DateInputField from "./dateInputField";
+import { useState } from "react";
 
 const Hero = () => {
-  const [checkInDate, setCheckInDate] = useState(null);
-  const [checkOutDate, setCheckOutDate] = useState(null);
+  const [checkInDate, setCheckInDate] = useState(new Date());
+  const [checkOutDate, setCheckOutDate] = useState(new Date());
 
   return (
     <section className="grid lg:grid-cols-2 gap-y-8 lg:gap-x-24 2xl:gap-x-36 3xl:gap-x-56 items-center mt-9 xl:mt-12">
@@ -31,35 +30,18 @@ const Hero = () => {
         <div className="grid grid-cols-2 w-full gap-4 mx-auto">
           {/* Check-in section */}
           <div className="col-span-2 sm:col-span-1">
-            <label className="block text-sm font-semibold text-gray-700 mb-1">
+            <label className="block text-sm 3xl:text-2xl font-semibold text-gray-700 mb-1">
               Check-out:{" "}
             </label>
-            <DatePicker
-              selected={checkOutDate}
-              onChange={(date) => setCheckOutDate(date)}
-              dateFormat="dd/MM/yyyy"
-              placeholderText="Check-out Date"
-              calendarClassName="p-0 w-full"
-              showIcon
-              calendarIconClassname="absolute !pt-2.5 right-0"
-              className="w-full !pr-32 lg:!pr-20    font-left !py-1.5 !pl-2 items-center font-light border border-gray-300 placeholder-placeColor rounded focus:outline-none"
-            />
+            <DateInputField date={checkInDate} setDate={setCheckInDate} />
           </div>
+
           {/* Check-out section */}
           <div className="col-span-2 sm:col-span-1">
-            <label className="block text-sm font-semibold text-gray-700 mb-1">
+            <label className="block text-sm 3xl:text-2xl font-semibold text-gray-700 mb-1">
               Check-out:{" "}
             </label>
-            <DatePicker
-              selected={checkOutDate}
-              onChange={(date) => setCheckOutDate(date)}
-              dateFormat="dd/MM/yyyy"
-              placeholderText="Check-out Date"
-              calendarClassName="p-0 w-full"
-              showIcon
-              calendarIconClassname="absolute !pt-2.5 right-0"
-              className="w-full !pr-32 lg:!pr-20    font-left !py-1.5 !pl-2 items-center font-light border border-gray-300 placeholder-placeColor rounded focus:outline-none"
-            />
+            <DateInputField date={checkOutDate} setDate={setCheckOutDate} />
           </div>
 
           <Button variant="gradient" className="col-span-2 gap-x-2 3xl:gap-x-3">
